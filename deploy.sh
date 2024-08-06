99669c9ec84643a6a607ea0f0b1d1fe8372643c4
jeff <>
2024-08-06 15:36:02 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,11 @@
 workingDir="/usr/project/koaServer"
 buildDir="/usr/build/koaServer"
 echo "Deploying to server..."
-cp -rf $workingDir  $buildDir
+# 判断目录是否存在，如果不存在则创建目录
+if [ ! -d "$buildDir" ]; then
+  mkdir -p "$buildDir"
+fi
+cp -rf $workingDir  $buildDir >> /dev/null
 if [ $? -eq 0 ]; then
   echo "Deployed successfully"
   exit 0
