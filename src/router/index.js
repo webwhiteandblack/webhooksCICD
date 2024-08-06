6b0e55260df919e2ce75012979cc6168f166ffe5
jeff <>
2024-08-06 16:16:01 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 const Router = require('@koa/router');
 const webhookRouter=require("./webhookRouter.js")
 const router = new Router();
-router.get(webhookRouter.path,webhookRouter.controller)
+router.post(webhookRouter.path,webhookRouter.controller)
 
 module.exports=router
