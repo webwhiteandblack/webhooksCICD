c7deb4cbd53e2e66f3e23ca444c62a80ef606f1b
jeff <>
2024-08-06 14:24:30 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+workingDir="usr/project/"
+buildDir="/usr/build/"
+echo "Deploying to server..."
+mv -f $buildDir/build/koaServer/dist  $workingDir/project/koaServer
+if [ $? -eq 0 ]; then
+  echo "Deployed successfully"
+  exit 0
+else
+  echo "Failed to deploy"
+  exit 1
+fi
